342bf52a4f657cc441e187c5ab5a3bb4bf110a92
AlexeiRadionov <radionov-aleksei@mail.ru>
2019-03-18 20:54:12 +0600

    добавил в модель метод получения баланса пользователя

--- models/TransactionsForm.class.php
+++ models/TransactionsForm.class.php
@@ -7,6 +7,17 @@
 		    return $users;
 		}
 
+		public function getBalance($user) {
+			$sql = "SELECT sum FROM users WHERE userName = '$user'";
+		    $balance = $this -> getAssocResult($sql);
+		    if (!empty($balance)) {
+		    	$response["result"] = 1;
+				$response["balance"] = $balance[0]['sum'];
+		    }
+		    
+		    return json_encode($response);
+		}
+
 		public function template() {
 			include '../Twig/Autoloader.php';
 			Twig_Autoloader::register();
