837438abe6a6d9d77e5f7a65e9c84b9f0b0b628a
AlexeiRadionov <radionov-aleksei@mail.ru>
2019-03-18 20:52:58 +0600

    добавил отображение баланса при выборе пользователя

--- public/js/engine.js
+++ public/js/engine.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	$('.start').on('click', function(){
-		var sender = $('.sender').val();
-		var recipient = $('.recipient').val();
-		var sum = $('.sum').val();
+		var sender = $('#sender').val();
+		var recipient = $('#recipient').val();
+		if ($('.sum').val() != '') {
+			var sum = $('.sum').val();
+		}
         
         $.ajax({
             url: "/startTransaction/",
@@ -21,14 +23,40 @@ $(document).ready(function() {
                     alert("Что-то пошло не так...");
             },
             error: function() {
-            	alert("Ошибка. Возможно у отправителя недостаточная сумма.");
+            	alert("Ошибка. Возможно у отправителя недостаточная сумма" + 
+            		"\n или не заполнены все поля");
             }
-        })
+        });
+    });
+
+    $('.user').on('change', function() {
+        var user = this.value;
+        var statusUser = $(this).attr('id');
+       
+        $.ajax({
+            url: "/index/balance/",
+            type: "POST",
+            data:{
+                user: user,
+            },
+            dataType : "json",
+            success: function(answer){
+                if(answer.result == 1) {
+                    showBalance(answer, statusUser);
+                } else
+                    alert("Что-то пошло не так...");
+            },
+            error: function() {
+            	alert("Ошибка");
+            }
+        });
     });
 });
 
 function resultTransaction(data) {
 	var str = '';
+	var balanceSender = '<p>Баланс: ' + data.sumSend + '</p>';
+	var balanceRecipient = '<p>Баланс: ' + data.sumRecipient + '</p>';
 
 	str += '<p>Отправитель: ' + data.sender + 
 	'</p><p>Баланс: ' + data.sumSend + 
@@ -36,4 +64,17 @@ function resultTransaction(data) {
 	'</p><p>Баланс: ' + data.sumRecipient + '</p>';
 	
 	$('.result').html(str);
+	$('.balanceSender').html(balanceSender);
+	$('.balanceRecipient').html(balanceRecipient);
+}
+
+function showBalance(data, statusUser) {
+	var str = '';
+
+	str += '<p>Баланс: ' + data.balance + '</p>';
+	if (statusUser == 'sender') {
+		$('.balanceSender').html(str);
+	} else if (statusUser == 'recipient') {
+		$('.balanceRecipient').html(str);
+	}
 }
